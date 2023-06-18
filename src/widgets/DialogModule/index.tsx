163656880a8feb94901f49fa6e77b879a3e9b0e5
Marat252552 <117304418+Marat252552@users.noreply.github.com>
2023-06-18 21:53:36 +0300

    3.2

--- src/widgets/DialogModule/index.tsx
+++ src/widgets/DialogModule/index.tsx
@@ -2,23 +2,23 @@ import MessageInput from '../../UI/MessageInput'
 import styles from './lib/styles.module.css'
 import { useAppSelector } from '../../state/hooks'
 import TextMessage from '../../entities/TextMessage'
-import {useMemo} from 'react'
 import HeaderModule from './elements/HeaderModule'
 
 
-const DialogModule = ({setNavbarActive}: {setNavbarActive: React.Dispatch<React.SetStateAction<boolean>>}) => {
+const DialogModule = ({ setNavbarActive }: { setNavbarActive: React.Dispatch<React.SetStateAction<boolean>> }) => {
 
     let { currentDialog } = useAppSelector(state => state.dialogsReducer)
     let { room_id, name } = currentDialog
     let { messages: allMessages } = useAppSelector(state => state.messagesReducer)
 
-    let messages = useMemo(() => allMessages.filter(message => {
+    let messages = allMessages.filter(message => {
         return message.room_id === room_id
-    }), [allMessages])
+    })
+
 
     return <div className={styles.container}>
-        
-        <HeaderModule setNavbarActive={setNavbarActive} name={name}/>
+
+        <HeaderModule setNavbarActive={setNavbarActive} name={name} />
 
         <div className={styles.chat_module_container}>
             <div className={styles.chat_module}>
@@ -26,7 +26,6 @@ const DialogModule = ({setNavbarActive}: {setNavbarActive: React.Dispatch<React.
                     {messages.map(message => {
                         return <TextMessage key={message.message_id} message={message} />
                     })}
-
                 </div>
                 <MessageInput room_id={room_id} />
             </div>
